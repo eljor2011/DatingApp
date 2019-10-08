f561282388ee89d1add0a5c73026fce0fd38d1c5
jorge luis quintana suarez <jor2011@hotmail.com>
2019-10-07 20:00:48 -0500

    Api 1

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength = 4, ErrorMessage ="Especifique una contraseña entre 4 y 8 caracteres")]
+        public string Password { get; set; }
+
+    }
+}
